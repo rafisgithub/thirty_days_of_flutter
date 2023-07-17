c9196d3bab4b7bcdf5091768ab016bb6c19f4fd4
Rafi <smrafi120@gmail.com>
2023-07-17 16:06:53 +0600

    Dart data types and Scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  double pi = 3.1416;
+  String name = "Rafi Ahmed";
+  bool isMale = true;
+  num temp = 30.2;
+  var day = 1;
+  var y = 3.14;
+  var x = "This is a varibale.";
+
+  // const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to  $days of flutter.by $name"),
+        ),
+      ),
+      drawer: Drawer(
+
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:thirty_days_of_flutter/home_page.dart';
 
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter."),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
