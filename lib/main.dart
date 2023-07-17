5c344bcc30c64ad992dd877726829e44508b5718
Rafi <smrafi120@gmail.com>
2023-07-17 19:31:35 +0600

    SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:thirty_days_of_flutter/pages/login_page.dart';
+import 'package:thirty_days_of_flutter/utilis/routes.dart';
 import 'pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(
@@ -22,8 +23,9 @@ class MyApp extends StatelessWidget {
       // initialRoute: "/home",
       routes: {
         "/" : (context) => LoginPage(),
-        "/home" : (context) => HomePage(),
-        "/login" : (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        // "/login" : (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:thirty_days_of_flutter/pages/home_page.dart';
+import 'package:thirty_days_of_flutter/utilis/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,59 +8,77 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset('assets/images/login_image.png',
-          fit: BoxFit.cover,
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/login_image.png',
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Text(
+                "Welocome",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border:OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        hintText: "Enter User Name",
+                        labelText: "User name",
+                      ),
+                    ),
+                    SizedBox(height: 14,),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        hintText: "Enter Password",
+                        labelText: "User Password",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  onPrimary: Colors.white,
+                  elevation: 10,
+                  minimumSize: Size(120, 37),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+              ),
+            ],
           ),
-          SizedBox(
-            height: 22,
-          ),
-          Text("Welocome",
-            style: TextStyle(
-                fontSize:24,
-              fontWeight: FontWeight.bold
-            ),
-          ),
-         Padding(
-           padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 30),
-           child: Column(
-             children: [
-               SizedBox(height: 20.0,),
-               TextField(
-                 decoration: InputDecoration(
-                   hintText: "Enter User Name",
-                   labelText: "User name",
-                 ),
-               ),
-               TextField(
-                 obscureText: true,
-                 decoration: InputDecoration(
-                   hintText: "Enter Password",
-                   labelText: "User Password",
-                 ),
-               ),
-             ],
-           ),
-         ),
-          SizedBox(height: 20,),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.pink,
-            onPrimary: Colors.white,
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(7),
-            )
-          ),
-          child: Text("login"),
-          onPressed: (){
-            print("HI");
-          },
-        ),
-        ],
-      )
-    );
+        ));
   }
 }
